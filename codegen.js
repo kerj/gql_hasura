c3f01dc5922ac27fb77e324f39f412abfb385c1d
Justin <4635401+kerj@users.noreply.github.com>
2023-02-14 15:53:26 -0800

    add codegen, include gql schema, update requests to use axios

---
+++ codegen.js
@@ -0,0 +1,30 @@
+module.exports = {
+  overwrite: true,
+  generates: {
+    "./src/generated/graphql.tsx": {
+      schema: [
+        {
+          "http://localhost:8080/v1/graphql": {},
+        },
+      ],
+    //   documents: ["./src/*.graphql"],
+      plugins: [
+        "typescript",
+        // "typescript-operations",
+        "typescript-react-apollo",
+      ],
+      config: {
+        preResolveTypes: true,
+        skipTypename: false,
+        withHooks: true,
+        withHOC: false,
+        withComponent: false,
+        enumsAsTypes: true,
+        constEnums: true,
+        reactApolloVersion: 3,
+      },
+    },
+  },
+};
+
+// commmented out lines should be used when there are queries/mutations defined from the client
