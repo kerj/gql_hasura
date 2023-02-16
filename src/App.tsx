05f849935a4c084eb315d6dd335e5336c3156429
Justin <4635401+kerj@users.noreply.github.com>
2023-02-16 10:24:45 -0800

    fix subscriptions url

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const http = new HttpLink({
   uri: "http://localhost:8080/v1/graphql"
 })
 const ws = new GraphQLWsLink(createClient({
-  url: "ws://localhost:8080/v1/subscriptions"
+  url: "ws://localhost:8080/v1/graphql"
   // connectionParams: {
   //   authToken: auth
   // }
